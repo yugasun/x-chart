333a37ed3fc5832f17f1b958a2d332da37573041
yugasun <yuga_sun@163.com>
2022-07-01 16:41:24 +0800

    fix: remove build time

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,11 @@
 <script lang="ts" setup>
-import { useTimeAgo } from '@vueuse/core';
 import Footer from '@/components/footer.vue';
-
-const date = '__DATE__';
-const timeAgo = useTimeAgo(date);
 </script>
 
 <template>
     <div id="app">
         <router-view />
-        <br />
-        <div class="build-time">Built at: {{ date }} ({{ timeAgo }})</div>
         <Footer />
     </div>
 </template>
-<style lang="scss" scoped>
-.build-time {
-    color: #999;
-}
-</style>
+<style lang="scss" scoped></style>
